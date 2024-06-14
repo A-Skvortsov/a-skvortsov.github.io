8944910b3cf92a98d77201c846dac6b9973224a2
Andrey Skvortsov <andreyskv49@gmail.com>
2024-06-14 12:36:22 -0400

    Integrating into Git
    Integrating my work into Git, previously was doing everything without source control. The files have quite a few changes and an added "scripts" folder with some JS related to resizing the background image with the window

---
+++ scripts/script1.js
@@ -0,0 +1,39 @@
+  // -----------
+  // Debugger that shows view port size. Helps when making responsive designs.
+  // -----------
+
+  function showViewPortSize(display) {
+    if(display) {
+      var height = $(window).height();
+      var width = $(window).width();
+      $('body').prepend('<div id="viewportsize" style="z-index:9999;position:fixed;top:40px;left:5px;color:#fff;background:#000;padding:10px">Height: '+height+'<br>Width: '+width+'</div>');
+      $(window).resize(function() {
+        height = $(window).height();
+        width = $(window).width();
+        $('#viewportsize').html('Height: '+height+'<br>Width: '+width);
+      });
+    }
+}
+//this is an example change
+$(document).ready(function() {
+	showViewPortSize(true);
+
+	$(window).resize(function(){
+		var nav_height = $('nav').height();
+    	var bg_width = window.innerWidth;
+    	var bg_height = window.innerHeight;
+
+    	if (bg_width > 1.6*bg_height) {
+    	//if viewport is longer than tall
+    	    $(".background img").css("height", "auto");
+    	    $(".background img").css("width", bg_width);
+    	} else {
+    	    $(".background img").css("width", "auto");
+    	    $(".background img").css("height", bg_height);
+    	}
+	});
+	$(window).trigger("resize");
+
+
+
+});
